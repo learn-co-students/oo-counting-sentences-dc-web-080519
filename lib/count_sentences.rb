c0dacf7172538713961d07fcbabf22713abb9a6b
Pete Hanner <petehanner@gmail.com>
2019-08-07 17:12:56 -0400

    String instance methods

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
+  def last_char(char)
+    return self[-1] == char
+  end
 
   def sentence?
-    
+    last_char(".")
   end
 
   def question?
-
+    last_char("?")
   end
 
   def exclamation?
-
+    last_char("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?] /)
+    sentences.count
   end
 end
